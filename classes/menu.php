d3ac20b88b89108357947afc3731f603d6c69609
main1015 <446740010@qq.com>
2015-03-29 17:17:56 +0800

    屏蔽“会员提现管理“链接

--- classes/menu.php
+++ classes/menu.php
@@ -49,7 +49,7 @@ class Menu
 			'会员管理'=>array(
 	    		'/member/member_list' => '会员列表',
 	     		'/member/group_list' => '会员组列表',
-	     		'/member/withdraw_list'=>'会员提现管理'
+//	     		'/member/withdraw_list'=>'会员提现管理'
 			),
 			'商户管理' => array(
 				'/member/seller_list' => '商户列表',
