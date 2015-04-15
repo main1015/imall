38b7aab47f4a38d9a1a16a10d864829dd4b72de5
main1015 <446740010@qq.com>
2015-04-15 17:10:01 +0800

    dev

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
-//header('location:./install/index.php');
-$iweb = dirname(__FILE__)."/lib/iweb.php";
-$config = dirname(__FILE__)."/config/config.php";
-require($iweb);
-IWeb::createWebApp($config)->run();
+header('location:./install/index.php');
 ?>
